8f4df8ffcb3937c21ca55d06724af25f48947ce9
Napus Samuanpho <napus.dev@gmail.com>
2025-08-23 21:46:19 +0700

    Remove module

--- backend/src/module/pre-order/dto/update-pre-order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePreOrderDto } from './create-pre-order.dto';
-
-export class UpdatePreOrderDto extends PartialType(CreatePreOrderDto) {}
